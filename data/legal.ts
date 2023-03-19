d356b41319b3d9616c0e61b94d8baf837d0c03cf
kunalkeshan <kunalkeshan12@gmail.com>
2023-03-19 19:38:14 +0530

    chore: privacy policy data added

---
+++ data/legal.ts
@@ -0,0 +1,42 @@
+/**
+ * Legal Information - Privacy Policy & Terms of Conditions
+ */
+
+export const PRIVACY_POLICIES = [
+	{
+		title: 'Information We Collect',
+		body: [
+			'Personal Information: When you register on our website, we may collect your name, email address, and other contact information.',
+			'Usage Data: We may collect information about your use of our website, such as your IP address, device type, browser type, and pages visited.',
+			'Cookies: We use cookies to improve your experience on our website and to personalize content and ads.',
+		],
+	},
+	{
+		title: 'How We Use Your Information:',
+		body: [
+			'To provide and improve our services',
+			'To personalize your experience on our website',
+			'To communicate with you about our services and updates',
+			'To comply with legal obligations',
+		],
+	},
+	{
+		title: 'Data Security',
+		body: [
+			'We take reasonable measures to protect your personal information from unauthorized access or disclosure.',
+		],
+	},
+	{
+		title: 'Third-Party Services',
+		body: [
+			'We may use third-party services to help us operate our website and provide our services. These services may have their own privacy policies, which we encourage you to review.',
+		],
+	},
+	{
+		title: 'Changes to this Policy',
+		body: [
+			'We may update this Privacy Policy from time to time. If we make material changes, we will notify you via email or through our website.',
+		],
+	},
+];
+export const TERMS_OF_CONDITIONS = [];
