9261872d2b965e3876771930d75fe87925aa3f40
kunalkeshan <kunalkeshan12@gmail.com>
2023-03-19 11:46:30 +0530

    chore: added how it works page

---
+++ pages/how-it-works.tsx
@@ -0,0 +1,8 @@
+import PublicLayout from '@/layouts/PublicLayout';
+import React from 'react';
+
+const HowItWorksPage = () => {
+	return <PublicLayout>HowItWorksPage</PublicLayout>;
+};
+
+export default HowItWorksPage;
