d695ea8f8b9739fecf07e76e02b7d59a99f9ad91
kunalkeshan <kunalkeshan12@gmail.com>
2023-03-19 12:03:10 +0530

    chore: about page general layout

--- components/landing/About.tsx
+++ components/landing/About.tsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import Image from 'next/image';
 
 const About = () => {
-	return <div>About</div>;
+	const ABOUT_INFO = [];
+
+	return (
+		<section>
+			<div>
+				<div>
+					<h2></h2>
+					<p></p>
+				</div>
+				<div>
+					<div>
+						<div>
+							<Image
+								src='/assets/svgs/about.svg'
+								width={100}
+								height={100}
+								alt='Skill-Matrix'
+								className='w-full h-auto object-contain'
+							/>
+						</div>
+					</div>
+					<div>
+						<ul></ul>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default About;
