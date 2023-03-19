c71ff8db21a523ea8d45aa933c12fdb2d0cdca38
kunalkeshan <kunalkeshan12@gmail.com>
2023-03-19 07:26:41 +0530

    chore: color setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,18 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#064EA4',
+        secondary: '#E1EDFB',
+        neutral: '#0F437F',
+      }
+    },
   },
   plugins: [],
 }
