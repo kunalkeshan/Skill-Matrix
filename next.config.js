d7206e1098a568a57db2c5c08f21317dac64009a
Kunal Keshan <kunalkeshan12@gmail.com>
2023-03-20 14:15:42 +0530

    chore: student profle setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- pages/student/[regNo]/index.tsx
+++ pages/student/[regNo]/index.tsx
@@ -1,13 +1,6 @@
 import { app, db } from '@/firebase';
 import { withSessionSsr } from '@/utils/withSession';
-import {
-	collection,
-	doc,
-	getDoc,
-	getDocs,
-	query,
-	where,
-} from 'firebase/firestore';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 import React from 'react';
 import {
 	InferGetServerSidePropsType,
@@ -16,8 +9,10 @@ import {
 } from 'next';
 import { getAuth, signOut } from 'firebase/auth';
 import { useRouter } from 'next/router';
-import {} from 'iron-session';
 import axios from 'axios';
+import Head from 'next/head';
+import PublicLayout from '@/layouts/PublicLayout';
+import Image from 'next/image';
 
 const auth = getAuth(app);
 
@@ -25,19 +20,40 @@ const StudentProfilePage: NextPage<
 	InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ student }) => {
 	const router = useRouter();
+
+	const handleLogout = async () => {
+		signOut(auth);
+		await axios.post('/api/student/logout');
+		router.push('/student/login');
+	};
+
 	return (
-		<div>
-			{student.name}
-			<button
-				onClick={async () => {
-					signOut(auth);
-					await axios.post('/api/student/logout');
-					router.push('/student/login');
-				}}
-			>
-				Sign Out
-			</button>
-		</div>
+		<>
+			<Head>
+				<title>{student.name}</title>
+			</Head>
+			<PublicLayout>
+				<section className='w-full min-h-screen'>
+					<header className='w-full min-h-[320px] bg-secondary1'>
+						<div>
+							<div className='max-w-md animate-float'>
+								<Image
+									src={student.avatar}
+									width={100}
+									height={100}
+									alt='Skill-Matrix'
+									className='w-full h-auto object-contain'
+								/>
+							</div>
+							<div>
+								<h1>{student.name}</h1>
+							</div>
+						</div>
+					</header>
+					<button onClick={handleLogout}>Sign Out</button>
+				</section>
+			</PublicLayout>
+		</>
 	);
 };
 
