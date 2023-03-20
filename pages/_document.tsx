d6eeef172a3e66feb7d336312f2720e4a52d18a6
Kunal Keshan <kunalkeshan12@gmail.com>
2023-03-20 09:33:55 +0530

    chore: smooth scroll added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html
+			lang='en'
+			className='scroll-smooth'
+			style={{ scrollBehavior: 'smooth' }}
+		>
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
 }
